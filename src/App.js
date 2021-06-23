60d6c6c1d660e3054cbf7c0965afd864e11713ea
Ross Foy <hamzaoflouisville@gmail.com>
2021-06-23 16:42:06 -0400

    update the project to be a working React app.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return(
+      <h1>
+        Your current hand of cards!
+      </h1>
+    )
+  }
+}
+
+export default App;
