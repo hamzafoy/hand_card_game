493a76a6035fcc0a3aa6df8d025cc4c067d96b83
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-18 03:57:26 -0400

    set up a very basic UI. I will be setting out to learn more and implement PhaserJS.

--- src/PlayersHand/PlayersHand.js
+++ src/PlayersHand/PlayersHand.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PlayersHand.css'
 import { hamzaFoy } from '../Logic/game_logic';
 import * as playingCards from './cards/index.js';
 
@@ -6,7 +7,7 @@ import * as playingCards from './cards/index.js';
 let cards = hamzaFoy.getCardInfo();
 
 let currentCards = cards.map(card =>
-<img src={playingCards.switchStatementForCards(card)} key={card.id} alt={card.suit + card.number} height="200px" width="125px"/>
+<img src={playingCards.switchStatementForCards(card)} key={card.id} alt={card.suit + card.number} height="125px" width="75px"/>
 );
 
 class PlayersHand extends React.Component {
@@ -14,10 +15,10 @@ class PlayersHand extends React.Component {
   render() {
     return(
       <>
-        Your current hand of cards!
-        <div>
+        <div className="handful-of-cards">
           {currentCards}
         </div>
+        <h2>Your current hand of cards!</h2>
       </>
     )
   }
